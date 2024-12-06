a2bc46c2bb2e8ebdcc720ade207324f377fe91c7
LuisFelipeZ <luisfelpzamorano@gmail.com>
2024-12-05 20:13:37 -0500

    reestructuracion main y app

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Root from "./Layouts/Root.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx"; // Importa el nuevo App.jsx
 import { Provider } from "react-redux"; // Importa el Provider
-import store from "./Store/Store.js"; // Importa el store de Redux
-import Mangas from "./Pages/Mangas.jsx";
-import Manager from "./Pages/Manager.jsx";
-import EditChapter from "./Pages/EditChapter.jsx";
-import AdminPanel from "./Pages/AdminPanel.jsx"; // Importa la nueva página
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "mangas",
-        element: <Mangas />,
-      },
-      {
-        path: "manager", // Ruta para Manager
-        element: <Manager />, // Componente Manager
-      },
-      {
-        path: "edit-chapter", // Ruta para EditChapter
-        element: <EditChapter />, // Componente EditChapter
-      },
-      {
-        path: "admin-panel", // Nueva ruta para AdminPanel
-        element: <AdminPanel />, // Componente AdminPanel
-      },
-    ],
-  },
-]);
+import store from "./Store/Store"; // Importa el store de Redux
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}> {/* Envuelve toda la aplicación con Provider */}
-      <RouterProvider router={router} />
+    <Provider store={store}>
+      <App /> {/* Usa el nuevo App.jsx */}
     </Provider>
   </StrictMode>
 );
