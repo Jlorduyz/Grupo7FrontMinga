a65bdeb6df85c07bea6331f883ac3f4cdd4a0eb8
MauricioYPS <yepes060@gmail.com>
2024-12-09 18:56:06 -0500

    Solucion problemas de renderizado

--- src/Components/Routes/PrivateUpdAuthor.jsx
+++ src/Components/Routes/PrivateUpdAuthor.jsx
@@ -2,9 +2,10 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 function PrivateUpdAuthor({children}) {
-    const token = useSelector(state => state.authStore?.token);
-    const role = useSelector(state => state.authStore.user?.role);
-
+    const token = localStorage.getItem("token");
+    const role = (JSON.parse(localStorage.getItem("user")).role)
+    console.log("role", role);
+    
     if (token && role !== 1)
         return <Navigate to="/home" replace></Navigate>
     else 
